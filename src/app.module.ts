c7a1294630beaae304620327b1db3714cfb65b77
Dani Canelo <78096392+danicanelo@users.noreply.github.com>
2024-03-03 09:40:12 +0100

    delete comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,6 @@ import { environment } from './environments/environment';
 import { UsersModule } from './user/users.module';
 import { YunukisModule } from './yunuki/yunukis.module';
 
-// Este fichero se encarga de importar y configurar los módulos de toda la aplicación.
-
 @Module({
   imports: [
     TypeOrmModule.forRoot(environment.typeOrmModuleOptions),

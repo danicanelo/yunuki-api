5b64904909952414561c68d7fc35b25b00e67ece
Daniel Canelo Soria <dcanelo@hiberus.com>
2025-08-25 14:09:35 +0200

    update: email and password validations

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
@@ -5,7 +6,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  }));
   await app.listen(3000);
 }
 bootstrap();
--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,17 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+/* eslint-disable prettier/prettier */
+import { IsNotEmpty } from 'class-validator';
+import { isStrongPassword } from 'src/common/decorators/is-strong-password.decorator';
+import { isCustomEmail } from 'src/common/decorators/custom-email.decorator';
 
 export class CreateUserDto {
-  @IsNotEmpty()
+  @IsNotEmpty({ message: 'El nombre de usuario es obligatorio.' })
   username: string;
-  @IsNotEmpty()
-  @IsEmail()
+
+  @IsNotEmpty({ message: 'El correo electrónico es obligatorio.' })
+  @isCustomEmail()
   email: string;
-  @IsNotEmpty()
+
+  @IsNotEmpty({ message: 'La contraseña es obligatoria.' })
+  @isStrongPassword()
   password: string;
 }
